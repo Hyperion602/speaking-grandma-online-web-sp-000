42882d943031a77cfd40e3382941ffe71fbb74b2
James Harris <mharris9608@yahoo.com>
2020-12-15 15:20:03 +0000

    Automatically backed up by Learn

---
+++ grandma.rb
@@ -0,0 +1,13 @@
+def speak_to_grandma(greeting)
+  if greeting != greeting.upcase
+    return "HUH?! SPEAK UP, SONNY!"
+  elsif greeting == greeting.upcase
+    return "NO, NOT SINCE 1938!"
+  elsif greeting == "I LOVE YOU GRANDMA!"
+    return "I LOVE YOU TOO PUMPKIN!"
+  else
+  end
+end
+
+
+    
